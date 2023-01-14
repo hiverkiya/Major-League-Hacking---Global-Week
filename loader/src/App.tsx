8e8ccf9e0186fcbb13b51b464b0cf4f661af1cea
hiverkiya <hiverkiya@gmail.com>
2023-01-14 16:42:49 +0530

    loader

---
+++ loader/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LoadingIcons from "react-loading-icons";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <LoadingIcons.Bars />
+      </header>{" "}
+    </div>
+  );
+}
+
+export default App;
