93aa12ed33c6cbd5c8c386f132344215e4cfdab7
hiverkiya <hiverkiya@gmail.com>
2023-01-15 09:34:53 +0530

    faq page

--- faq-page/src/App.js
+++ faq-page/src/App.js
@@ -1,55 +1,29 @@
 import React from "react";
-import "./App.css";
-class App extends React.Component {
-  // Constructor
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
-
-  // ComponentDidMount is used to
-  // execute the code
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
-      });
-  }
-  render() {
-    const { DataisLoaded, items } = this.state;
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1 className="p-2"> Pleses wait some time.... </h1>{" "}
-        </div>
-      );
-
-    return (
-      <div className="App">
-        <h1 className="p-2 text-black-300 bg-blue-400 mb-10">
+function FAQ() {
+  return (
+    <div className="grid h-screen place-items-center bg-gradient-to-tr from-cyan-400 to-yellow-400">
+      <div className=" rounded-2xl bg-yellow-200 text-lg font-semibold uppercase p-4 flex-reverse justify-center items-center text-center flex-auto">
+        <div className="animate-pulse mb-5  p-4 bg-green-300 rounded-xl">
           {" "}
-          Fetch data from an api in react{" "}
-        </h1>{" "}
-        {items.map((item) => (
-          <ol
-            className="space-y-4 mt-2 bg-yellow-400 rounded-xl "
-            key={item.id}
-          >
-            User_Name: {item.username}, Full_Name: {item.name}, User_Email:{" "}
-            {item.email}
-          </ol>
-        ))}
+          FAQ page for the Hackerabad Guild
+        </div>
+        <div>
+          {" "}
+          Are people in the guild friendly?{" "}
+          <span className="font-bold text-red-500 ">Yes they are!</span>
+        </div>
+        <div>
+          {" "}
+          How can I join your guild?{" "}
+          <span className="font-bold text-red-500 ">Check our Discord</span>
+        </div>
       </div>
-    );
-  }
+      <div className="text-7xl text-red-400 p-4 bg-cyan-200 rounded-2xl">
+        Made during MLH
+      </div>
+    </div>
+  );
 }
 
-export default App;
+export default FAQ;
